6976c9759776f8d46d8e8f3a3f309a8e097121f5
bar87 <bar8870@gmail.com>
2021-11-22 11:16:13 +0200

    first commit

---
+++ my_mat.c
@@ -0,0 +1,33 @@
+#include <my_mat.h>
+
+int matUsre(int i , int j){
+int arr[10][10]; 
+for( i = 0 ; i < 10 ; i++)
+{
+    for(j = i ; j < 10 ; j++)
+    {
+        printf("Enter value for disp[%d][%d]:", i, j);
+        scanf("%d", &arr[i][j]);
+    }
+}
+}
+
+int isRoute(int i , int j){
+
+
+}
+
+
+
+int smallRoute(int i , int j ){
+for(int k = 0 ; k < 10 ; k++)
+{
+   for (int i = 0 ; i < 10 ; i++)
+    {
+        for(int j = i ; j < 10 ; j++)
+        {
+
+        }
+    }
+}
+}
---
+++ my_mat.h
@@ -0,0 +1,8 @@
+/* The program receives from the user the matrix values 10X10 */
+int matUsre(); 
+/* The program receives from the user the values i and j and prints "True" if there is a trajectory from i
+To j .other, print "False. when true = 1 ; fals = 0 ;  */
+int isRoute(int,int);
+/*The program captures the i and j values from the user and prints the shortest route from
+i to j, if the program route does not exist Print 1. */
+int smallRoute(int, int);
