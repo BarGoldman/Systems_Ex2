958f8916f6d34024ecbf52e901e7cac7ff1d15e4
BarGoldman <bar8770@gmail.com>
2021-11-22 23:05:29 +0200

    version 2

--- my_mat.c
+++ my_mat.c
@@ -2,7 +2,7 @@
 #include "my_mat.h"
 #define N 10
 #define num 300000
-
+/* The program receives from the user the matrix values 10X10 */
 void A(int mat[][N] ,int n)
 { 
     int b;
@@ -10,21 +10,21 @@ void A(int mat[][N] ,int n)
     {
         for(int j = 0 ; j < N ; j++)
         {
-            scanf("%d", &b);
-            mat[i][j] = b ;
+            scanf("%d", &b); //Receives value from the user
+            mat[i][j] = b ; //Defines the value of the mat[i][j] 
         }
     }
 }
-
+/*change the matrix to the shortest route from i to j*/
 void Algo(int mat[][N] ,int n)
 {
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < N; j++)
         {
-            if(i != j && mat[i][j] == 0)
+            if(i != j && mat[i][j] == 0) //if i ans j different and the route does not exist beetwin i and j
             {
-                mat[i][j] = num; 
+                mat[i][j] = num; //valu of mat[i][j] is num = 300000
             }
         }
     }
@@ -34,52 +34,56 @@ void Algo(int mat[][N] ,int n)
         {
             for(int j = 0 ; j < N ; j++)
             {
-                if(mat[i][k]+mat[k][j] < mat[i][j])
+                if(mat[i][k]+mat[k][j] < mat[i][j]) 
                 {
-                    mat[i][j] = mat[i][k]+mat[k][j]; 
+                    mat[i][j] = mat[i][k]+mat[k][j]; // mat[i][j] = the shortest route from i to j, 
                 }
             }
         }
     }
 }
-
-void B(int mat[][N] , int a , int b){
-    int temp[N][N];
-    for (int i = 0; i < N ; i++)
+/* The program receives from the user the values i and j and prints "True" if 
+ there is a trajectory from i
+ To j .other, print "False".*/ 
+void B(int mat[][N] , int a , int b)
+{
+    int temp[N][N]; // new matrix NxN
+    for (int i = 0; i < N ; i++) 
     {
         for (int j = 0; j < N ; j++)
         {
-            temp[i][j] = mat[i][j];
+            temp[i][j] = mat[i][j];//copy the same matrix mat
         }
     }
     Algo(temp ,N);
-    if (temp[a][b]  != num && a != b)
+    if (temp[a][b] != num && a != b) 
     {
-        printf("True\n");
+        printf("True\n"); //print true if there is a trajectory from i To j
     } 
     else
     {
-        printf("False\n");
+        printf("False\n");//print false if there is not trajectory from i To j
     }   
 }
-
+/*The program captures the i and j values from the user and prints the shortest route from
+i to j, if the program route does not exist Print -1*/
 void C(int mat[][N] , int a , int b)
 {
-    int temp[N][N];
+    int temp[N][N];// new matrix NxN
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < N; j++)
         {
-            temp[i][j] = mat[i][j];
+            temp[i][j] = mat[i][j];//copy the same matrix mat
         }
     }
     Algo(temp,N); 
     if (temp[a][b] == num || a == b)
     {
-        printf("%d\n",-1);
+        printf("%d\n",-1); // print -1 if the route does not exist 
     }      
     else
     {
-        printf("%d\n",temp[a][b]);
+        printf("%d\n",temp[a][b]); //prints the shortest route from i to j,
     }   
 }
--- my_mat.h
+++ my_mat.h
@@ -3,8 +3,8 @@
 /* The program receives from the user the matrix values 10X10 */
 void A(int [][N] ,int);
 /* The program receives from the user the values i and j and prints "True" if there is a trajectory from i
-To j .other, print "False. when true = 1 ; fals = 0 ;  */
+To j .other, print "False".  */
 void B(int [][N] , int , int);
 /*The program captures the i and j values from the user and prints the shortest route from
-i to j, if the program route does not exist Print 1. */
+i to j, if the program route does not exist Print -1. */
 void C(int [][N] , int , int);
